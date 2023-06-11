aa275205af3575923a7d96ad195429e3b782c4d6
Болдырев Андрей Романович <134797513+ANBOLL@users.noreply.github.com>
2023-06-11 12:45:28 +0300

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,1 +1,12 @@
-
+<?php
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $dbname = "contact";
+    $conn=mysqli_connect($servername,$username,$password,"$dbname");
+    
+      if(!$conn)
+        {
+          die('Could not Connect MySql Server:' .mysql_error());
+        }
+?>
